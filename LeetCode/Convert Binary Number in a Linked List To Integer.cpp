0fb3a2dd7eae39c7f4c7f7a75b18239d07f90dc8
AdmiJW <jamweg22@gmail.com>
2020-11-01 16:28:35 +0800

    Added Convert Binary Number in a Linekd List to Integer

---
+++ LeetCode/Convert Binary Number in a Linked List To Integer.cpp
@@ -0,0 +1,70 @@
+
+//https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+/*
+ * 	THis is a bit manipulation - Linked list problem.
+ *
+ * 	When we traverse the linked list, we traverse from rightmost to leftmost. This means the digit we met earlier in the
+ * 	linked list should be increase its digit position by 1 when we traverse to the next node.
+ * 	Recall from binary representation, each position is 2^1, 2^2, 2^3 etc. This means that we have options:
+ *
+ * 	>	Use leftshift operator, which basically shift all bits to left by one
+ * 	>	Use *2 every time we traverse the node.
+ *
+ */
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+	
+};
+
+
+class Solution {
+public:
+
+	//	BIT MANIPULATION ITERATIVE METHOD 
+
+	int getDecimalValue(ListNode* head) {
+		int res = 0;
+
+		while (head) {
+			res <<= 1;
+			res += head->val;
+			head = head->next;
+		}
+
+		return res;
+	}
+
+	//	RECURSION METHOD
+
+	int getDecimalValueRecursion(ListNode* head) {
+		return getDecimalValueRecursion(head, 0);
+	}
+	int getDecimalValueRecursion(ListNode* head, int val) {
+		if (!head) return val;
+
+		val <<= 1;
+		val += head->val;
+		
+		return val;
+	}
+
+	//	MATH METHOD (*2 every time)
+
+	int getDecimalValueMath(ListNode* head) {
+		int res = 0;
+
+		while (head) {
+			res *= 2;
+			res += head->val;
+			head = head->next;
+		}
+
+		return res;
+	}
+};
--- LeetCode/Number of Longest Increasing Subsequence.cpp
+++ LeetCode/Number of Longest Increasing Subsequence.cpp
@@ -2,6 +2,39 @@
 #include <cmath>
 
 
+/*
+ * 	This is a Dynamic Programming Problem (DP), and possibly more advanced, segment tree problem.
+ *
+ * 	Whenever we see the maximum problem and also subsequence, we can almost get that this might be an DP question.
+ *
+ * 	For each element in the array, it might be able to connect with one among previous subsequences available in the
+ * 	array. To achieve this, we can iterate backwards one by one to check the suitable candidates to form subarray with.
+ *
+ * 	Now, let's say we found a suitable candidate. How do we know once we connected the element with previous subsequnce,
+ * 	what is the length of the final subsequence, and how many subsequences of such length is created?
+ * 	This is where DP comes in. With DP, we already assume we know the answers to previous subproblems. That is,
+ * 		>	The maximum length subsequence that can be formed with this element as the ending element
+ * 		>	The number of subsequences that can be formed with such maximum length, and this element as the ending element
+ *
+ * 	When we form subsequence with said previous element,
+ * 		>	The length of resulting subsequence would be:				prev max length + 1 (current element)
+ * 		>	The count of resulting subsequence would actualy be same; How many subsequences previously, we append an element
+ * 			to all of them. The count doesn't change.
+ *
+ * 	With those information in power, when we finished iteration backwards, we shall already know two things:
+ * 		>	The length of maximum subsequence ending with current element
+ * 		>	The number of subsequences with this maximum length, formed with current element as last element.
+ * 	Which is exactly the answer to the current subproblem of DP!
+ * 	Also, since DP does not directly solve the actual problem, We have to compare each subproblem answer to the global answer.
+ * 	THat is,
+ *
+ * 		If the current subproblem's maximum length is equal to global maximum length, then add the count of possible
+ * 		subsequences that can be formed into global count.
+ * 		Otherwise, if the current subproblem's maximum length is GREATER than global maximum length, we actually have to
+ * 		update the global maximum, reset the global count to the subproblem's count
+ */
+
+
 class Solution {
 public:
 
