1ce6a65315762044db8a4dbf605a1f2720ed6ad4
AdmiJW <jamweg22@gmail.com>
2021-06-15 23:12:02 +0800

    Added Matchsticks to Square

--- LeetCode/Matchsticks to Square.cpp
+++ LeetCode/Matchsticks to Square.cpp
@@ -1,6 +1,33 @@
 #include <vector>
 using namespace std;
 
+//https://leetcode.com/problems/matchsticks-to-square/
+/*
+ * 	This is a (Backtracking) DFS, potentially DP problem
+ *
+ * 	First of all, given a set of matches, one can check if it is even possible to form a square
+ * 	by first obtaining the sum of lengths, and see if it can be perfectly divisible by 4.
+ * 	Also, number of matches must exceed or equal 4.
+ *
+ * 	For DFS, each match must exactly belong to one group. Therefore we imagine a decision tree
+ * 	of N levels, and at each level, we will choose to make the match belong to either of 4 sides,
+ * 	say group1, group2, group3 and group4.
+ *
+ * 	You can check when we ran out of matches, whether g1,g2,g3 and g4 are all equal to target length.
+ * 	If yes, then definitely a square is formed.
+ *
+ * 	One fact is that given the divisible by 4 is established, if 3 sides are already formed, then the
+ * 	last side must be formable too! This may lead us for early termination by checking whether 3 sides
+ * 	had formed target length already or not.
+ *
+ * 	One trick is to sort matches descending order before recursion. This is because longer match will be
+ * 	used first, and if impossible, will lead to early termination which is good
+ *
+ * 	=============================
+ *
+ * 	There is a valid Dynamic Programming solution in leetcode, but it is considered harder to implement due to
+ * 	involvement of encoding states in integer32. If interested, one may visit the solution there
+ */
 
 class Solution {
 public:
