40d2fc0abe1f69f8ae83dcec4783b34b1798789d
AdmiJW <jamweg22@gmail.com>
2021-12-14 08:27:03 +0800

    Adeed Range Sum of BST

--- LeetCode/Range Sum of BST.cpp
+++ LeetCode/Range Sum of BST.cpp
@@ -4,34 +4,39 @@
 
 /*
 *   This is a BST traversal problem, which is easily solved using recursion or any iteration method of Stack or Queue
-* 
+*
 *   At each recursion call, we receive a node. Since we know the left and right boundaries, we check:
 *   If the node's value is in range, this node value must be included in sum.
 *   Then, we know left subtree all values must be less than this node value, and reverse for right subtree.
-* 
+*
 *   If the left boundary is still lower than the node value, then some node in left subtree may be valid node. Recurse
 *   left
 *   If the right boundary is still higher than the node value, then some node in right subtree may be valid node.
 *   Recurse right
-* 
+*
 *   ================================================================
-* 
+*
 *   The very same can be done iteratively, usign Stack (DFS) or Queue (BFS)
-* 
+*
 */
 
+
+
+
 struct TreeNode {
-int val;
-TreeNode* left;
-TreeNode* right;
-TreeNode() : val(0), left(nullptr), right(nullptr) {}
-TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
 
 class Solution {
 public:
+
+    // Recursive solution
     int rangeSumBST(TreeNode* root, int low, int high) {
         if (!root) return 0;
 
@@ -41,6 +46,9 @@ public:
         return sum;
     }
 
+
+
+    // Iterative DFS solution (Stack)
     int rangeSumBST2(TreeNode* root, int low, int high) {
         if (!root) return 0;
 
