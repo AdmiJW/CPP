068e2428901fe518fa466b391630329ac9aa211e
AdmiJW <jamweg22@gmail.com>
2021-12-29 10:48:55 +0800

    Modified Populating next Right Pointers in Each Node

--- LeetCode/Populating Next Right Pointers in Each Node.cpp
+++ LeetCode/Populating Next Right Pointers in Each Node.cpp
@@ -1,8 +1,8 @@
 
 #include <queue>
 
-//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 /*
  * 	This is a Tree Depth First Search/ Breadth First Search Problem
  *
@@ -26,29 +26,21 @@
  * 				( )(A)(B)( )
  *
  * 	Indeed. using Depth First Search, how would we know that A and B are supposed to be connected?
- * 	The solution is, we have a recursive function which will take in TWO, 2 parent nodes as arguments. This 2 parent nodes
- * 	know that the middle nodes have to be connected well!
+ * 	The idea is: By the time we reach A and B, both their parents should already be have their next pointer populated.
+ * 	In this case, A's parent next pointer will be pointing to B's parent.
  *
- * 	Basically, this recursive function will do following:
- *			if node is null return
- *			connect the left node's children
- *			connect the right node's children
- *			connect the left node's right to right node's left (The 'Gap' is now connected)
+ * 	Therefore, in recurson, what we do is:
+ * 		1. Connect left child to right child
+ * 		2. Connect right child to root's next node's right child. Like this:
  *
- *			recurse(left node's children)
- *			recurse(right node's children)
- *			recurse(left node's right & right node's left)     (This ensures that the layer below will always be connected)
+ * 			(root) ----> (next)
+ * 			/	\		 /	 \
+ * 		(  )   (A)     (B)   ( )
  *
- *	==============================================================================================
- *
- *	Introducing the technique to perform Level order traversal using the next pointer in each node
- *
- *	We will keep a leftmost pointer, which only stores the leftmost node in a level. Then, treet this node as parent node
- *	of children. We will conenct the children together.
- *	Now, do we have access to the node next to right children? Yes!
- *	We keep track of the current parent node, and we can access it's next node in level. The next node's left children
- *	is the node we have to connect!
+ * 			root.right.next = root.next.left;
  */
+
+
 
 class Node {
 public:
@@ -71,10 +63,8 @@ public:
         bfs.push(root);
 
         while (!bfs.empty()) {
-            //	Get Layer size
             int layerSize = bfs.size();
 
-            //	Ensure don't go over layer size!
             while (layerSize-- > 0) {
                 Node* n = bfs.front();
                 bfs.pop();
@@ -95,45 +85,18 @@ public:
     //	Depth First Search solution
     Node* connect2(Node* root) {
         //	Base case: no connection to do
-        if (!root || !root->left) return root;
+        if ( !root || !(root->left) ) return root;
 
         //	First 2 nodes need to manually conenct
         root->left->next = root->right;
 
-        recurse(root->left, root->right);
+        // To interconnect between nodes that don't share parents, use ROOT node's next pointer
+        if (root->next)
+            root->right->next = root->next->left;
+
+        connect2(root->left);
+        connect2(root->right);
         return root;
-    }
-
-    Node* connect3(Node* root) {
-        Node* leftmost = root;
-
-        while (leftmost) {
-            Node* curr = leftmost;
-            while (curr) {
-                connect(curr->left, curr->right);
-                if (curr->next)
-                    connect(curr->right, curr->next->left);
-                curr = curr->next;
-            }
-            leftmost = leftmost->left;
-        }
-        return root;
-    }
-
-private:
-    void recurse(Node* left, Node* right) {
-        if (!left) return;
-        connect(left->left, left->right);
-        connect(right->left, right->right);
-        connect(left->right, right->left);
-
-        recurse(left->left, left->right);
-        recurse(right->left, right->right);
-        recurse(left->right, right->left);
-    }
-    void connect(Node* left, Node* right) {
-        if (!left) return;
-        left->next = right;
     }
 };
 
