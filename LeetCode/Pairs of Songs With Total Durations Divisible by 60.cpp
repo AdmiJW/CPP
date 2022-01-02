3b0aad771469925568bc1d0e4162974f47c1bc09
AdmiJW <jamweg22@gmail.com>
2022-01-02 12:28:42 +0800

    Modified Pairs of SOngs With Total Durations Divisible by 60

--- LeetCode/Pairs of Songs With Total Durations Divisible by 60.cpp
+++ LeetCode/Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -23,14 +23,12 @@ using namespace std;
 class Solution {
 public:
 	int numPairsDivisibleBy60(vector<int>& time) {
-		int bucket[61] = {}; //One extra space for easier access
+		int bucket[60] = {}; 
 		int res = 0;
 
 		for (int n : time) {
 			int remainder = n % 60;
-			res += bucket[60 - remainder];
-
-			remainder = !remainder ? 60 : remainder; //	If remainder is 0, set to 60. So a new pair can find this one
+			res += bucket[ (60 - remainder) % 60 ];
 			++bucket[remainder];
 		}
 		return res;
